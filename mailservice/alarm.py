36e0eee04d0a33876763554f9318110153707e36
Johanna Blomstedt <johanna.blomstedt@prevas.se>
2022-02-23 13:45:39 +0100

    Added Alarm class for finding entries which require a response. Modified formatting of header in  Mailer class.

--- mailservice/alarm.py
+++ mailservice/alarm.py
@@ -5,10 +5,14 @@ sys.path.append('/home/tony/code/groundcheck/flaskproject')
 print(sys.path)
 from db import *
 from models import *
+from read_settings import *
 from sqlalchemy import or_
 import mail
+import time
 mailer = mail.Mailer()
 limit=1
+
+
 
 # get not passed controllEntries
 def get_not_passed_controllEntries():
@@ -17,12 +21,25 @@ def get_not_passed_controllEntries():
                 filter(ControlledEntry.handled==False).all()
 
 def get_expired_control_entries(time_now,allowed_timedelta_min):
-    return []
+    allowed_time=session.query(Settings).time_to_refresh
+    return session.query(ControlledEntry).join(TestSample).filter(TestSample.test_time-time_now>allowed_time).filter(ControlledEntry.handled==False).all()
+
+def get_control_errors():
+    return session.query(ControlledEntry).join(TestSample). \
+    filter(TestSample.error==True). \
+    filter(ControlledEntry.handled==False).all()
+
+
+        
+def failed_test_message(entry):
+    message = entry.name + ' '
+
 
 def send_alarm(entry):
         print(entry.description)
         try:
-            mailer.send_mail(entry.employee.manager,format_mail(entry))
+            mailer.send_mail(entry.employee.manager,format_mail(entry), "Autosober Alert")
+            set_entry_handled(entry)
             return True
         except:
             print("Failed to send mail. Entry not updated to 'handled' and will be sent later")
@@ -48,10 +65,14 @@ def main():
     unhandled_entries = []
     unhandled_entries.extend(get_not_passed_controllEntries())
     unhandled_entries.extend(get_expired_control_entries())
+    unhandled_entries.extend(get_control_errors())
+    
 
     for entry in unhandled_entries: 
         if send_alarm(entry):
             set_entry_handled(entry)
+
+
 
 if __name__ == "__main__":
     main()
